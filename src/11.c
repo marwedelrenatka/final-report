8f56085430eb21f8f80763dd69d110cbec24f3ee
marwedelrenatka <marwedelrenatka@gmail.com>
2025-03-16 03:05:19 +0000

    Add 11.c

---
+++ src/11.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        printf("The value of x is positive.\n");
+    } else {
+        printf("The value of x is not positive.\n");
+    }
+    return 0;
+}
